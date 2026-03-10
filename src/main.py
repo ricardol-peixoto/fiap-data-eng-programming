0ed1bbd3d4368061d522c90281bbed154fd1e5f6
ricardol-peixoto <riclimpe@gmail.com>
2026-03-10 00:23:23 +0000

    Refatoração Estágio 3.2: Injeção do pacote Pipeline no fluxo principal.

--- src/main.py
+++ src/main.py
@@ -6,72 +6,77 @@ from processing.transformations import Transformation
 import os
 from pathlib import Path
 
+from pipeline.pipeline import Pipeline
+
 def main():
     config = carregar_config()[0]
     app_name = config['spark']['app_name']
     print(f"Obtido o app name: {app_name}")
 
-    main_root_dir = Path(os.getcwd()) # Obtém o caminho atual. Será utilizado para apontar para os inputs/outputs de forma modular e agnóstica.
+    # main_root_dir = Path(os.getcwd()) # Obtém o caminho atual. Será utilizado para apontar para os inputs/outputs de forma modular e agnóstica.
 
     # print("Abrindo a sessao spark")
     spark = SparkSessionManager.get_spark_session(app_name=app_name)
 
-    data_handler = DataHandler(spark)
-    transformer = Transformation()
+    # data_handler = DataHandler(spark)
+    # transformer = Transformation()
     
-    print("Abrindo o dataframe de pedidos")
-    if "DATABRICKS_RUNTIME_VERSION" in os.environ:
-        path_pedidos = main_root_dir.parent / config['paths']['pedidos']
-    else:
-        path_pedidos = main_root_dir / config['paths']['pedidos']
+    # print("Abrindo o dataframe de pedidos")
+    # if "DATABRICKS_RUNTIME_VERSION" in os.environ:
+    #     path_pedidos = main_root_dir.parent / config['paths']['pedidos']
+    # else:
+    #     path_pedidos = main_root_dir / config['paths']['pedidos']
     
-    path_pedidos_str = str(path_pedidos)
-    print(f"Obtidos o path de pedidos: {path_pedidos_str}")
-    print("Carregando parâmetros de leitura das informações de pedidos")
-    compression_pedidos = config['file_options']['pedidos_csv']['compression']
-    header_pedidos = config['file_options']['pedidos_csv']['header']
-    separator_pedidos = config['file_options']['pedidos_csv']['sep']
-    pedidos = data_handler.load_pedidos(path=path_pedidos_str, compression=compression_pedidos, header=header_pedidos, sep=separator_pedidos)
-    print("Dados de pedidos carregados")
-    pedidos.show(5, truncate=False)
+    # path_pedidos_str = str(path_pedidos)
+    # print(f"Obtidos o path de pedidos: {path_pedidos_str}")
+    # print("Carregando parâmetros de leitura das informações de pedidos")
+    # compression_pedidos = config['file_options']['pedidos_csv']['compression']
+    # header_pedidos = config['file_options']['pedidos_csv']['header']
+    # separator_pedidos = config['file_options']['pedidos_csv']['sep']
+    # pedidos = data_handler.load_pedidos(path=path_pedidos_str, compression=compression_pedidos, header=header_pedidos, sep=separator_pedidos)
+    # print("Dados de pedidos carregados")
+    # pedidos.show(5, truncate=False)
 
-    print("Abrindo o dataframe de pagamentos")
-    if "DATABRICKS_RUNTIME_VERSION" in os.environ:
-        path_pagamentos = main_root_dir.parent / config['paths']['pagamentos']
-    else:
-        path_pagamentos = main_root_dir / config['paths']['pagamentos']
-        
-    path_pagamentos_str = str(path_pagamentos)
-    print(f"Obtidos o path de pagamentos: {path_pedidos_str}")
-    print("Carregando parâmetros de leitura das informações de pedidos")
-    compression_pagamentos = config['file_options']['pagamentos_json']['compression']
-    pagamentos = data_handler.load_pagamentos(path=path_pagamentos_str, compression=compression_pagamentos)
-    pagamentos.show(5, truncate=False)
+    # print("Abrindo o dataframe de pagamentos")
+    # if "DATABRICKS_RUNTIME_VERSION" in os.environ:
+    #     path_pagamentos = main_root_dir.parent / config['paths']['pagamentos']
+    # else:
+    #     path_pagamentos = main_root_dir / config['paths']['pagamentos']
 
-    print("Fazendo a junção dos dataframes")
-    pedidos_pagamentos = transformer.join_pedidos_pagamentos(pedidos, pagamentos)
+    # path_pagamentos_str = str(path_pagamentos)
+    # print(f"Obtidos o path de pagamentos: {path_pedidos_str}")
+    # print("Carregando parâmetros de leitura das informações de pedidos")
+    # compression_pagamentos = config['file_options']['pagamentos_json']['compression']
+    # pagamentos = data_handler.load_pagamentos(path=path_pagamentos_str, compression=compression_pagamentos)
+    # pagamentos.show(5, truncate=False)
 
-    print("Extraindo Relatório Final")
-    relatorio_final = transformer.relatorio(pedidos_pagamentos)
-    relatorio_final.show(10, truncate=False)
+    # print("Fazendo a junção dos dataframes")
+    # pedidos_pagamentos = transformer.join_pedidos_pagamentos(pedidos, pagamentos)
 
-    # Hora de Gravar o Relatório Final com os pedidos legítimos e que foram recusados.
-    print("Escrevendo o resultado em parquet")
-    # Nosso grupo usou mais de uma ferramenta para estruturar o Trabalho Final.
-    # Se estiver no Databricks, ele utilizará a variável 'DATABRICKS_RUNTIME_VERSION'
-    is_databricks = "DATABRICKS_RUNTIME_VERSION" in os.environ
+    # print("Extraindo Relatório Final")
+    # relatorio_final = transformer.relatorio(pedidos_pagamentos)
+    # relatorio_final.show(10, truncate=False)
 
-    if is_databricks:
-        # Caminho para o "Storage" gratuito do Databricks. 
-        # É necessário criar o volume (fiap-data-eng-programming) e o schema (data-programming-trab-final).
-        base_out = "/Volumes/workspace/fiap-data-eng-programming/data-programming-trab-final/"
-        print(f"Obtido o path de saída: {base_out}")
-        data_handler.write_parquet(df=relatorio_final, path=base_out)
-    else:
-        # Caminho para sua máquina local (pasta do projeto)
-        base_out = config['paths']['output']
-        print(f"Obtido o path de saída: {base_out}")
-        data_handler.write_parquet(df=relatorio_final, path=base_out)    
+    # # Hora de Gravar o Relatório Final com os pedidos legítimos e que foram recusados.
+    # print("Escrevendo o resultado em parquet")
+    # # Nosso grupo usou mais de uma ferramenta para estruturar o Trabalho Final.
+    # # Se estiver no Databricks, ele utilizará a variável 'DATABRICKS_RUNTIME_VERSION'
+    # is_databricks = "DATABRICKS_RUNTIME_VERSION" in os.environ
+
+    # if is_databricks:
+    #     # Caminho para o "Storage" gratuito do Databricks. 
+    #     # É necessário criar o volume (fiap-data-eng-programming) e o schema (data-programming-trab-final).
+    #     base_out = "/Volumes/workspace/fiap-data-eng-programming/data-programming-trab-final/"
+    #     print(f"Obtido o path de saída: {base_out}")
+    #     data_handler.write_parquet(df=relatorio_final, path=base_out)
+    # else:
+    #     # Caminho para sua máquina local (pasta do projeto)
+    #     base_out = config['paths']['output']
+    #     print(f"Obtido o path de saída: {base_out}")
+    #     data_handler.write_parquet(df=relatorio_final, path=base_out)    
+
+    pipeline = Pipeline(spark)
+    pipeline.run(config=config)
     
     spark.stop()
 
